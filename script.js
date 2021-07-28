e6c30427509ac6b7db90962ec421d6881ba699c3
JasmineBabaYara <jassyprincess1@gmail.com>
2021-07-27 22:36:49 -0700

    cart

---
+++ script.js
@@ -0,0 +1,22 @@
+const applebutton = document.getElementById('apple-btn');
+const apple = document.getElementById('apple');
+const banana = document.getElementById('banana');
+const carrot = document.getElementById('carrot');
+const desert = document.getElementById('desert');
+
+function addapple() {
+    apple.innerHTML='Apple';
+}
+
+function addbanana() {
+    banana.innerHTML='banana';
+}
+
+function addcarrot() {
+    carrot.innerHTML='carrot';
+}
+
+function adddesert() {
+    desert.innerHTML='desert';
+}
+
